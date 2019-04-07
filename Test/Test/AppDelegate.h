1f9f8bec49263a6bf00bb8c1a932cb3adea1805e
Rupanjan Sen <>
2019-04-07 17:47:39 +0530

    Test

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by rupanjan on 07/04/19.
+//  Copyright © 2019 Rupanjan Sen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Test/Test/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Test
+//
+//  Created by rupanjan on 07/04/19.
+//  Copyright © 2019 Rupanjan Sen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
